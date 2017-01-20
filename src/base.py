0f1a1f28274c8c18a1c4b4c7bc41de68a0e8dfaa
Zach Goldstein <zgoldstein@google.com>
2017-01-20 12:28:28 -0500

    Added error handling and diagnostics.

--- src/base.py
+++ src/base.py
@@ -1,7 +1,23 @@
 import logging
 
 
+class SingleLog(object):
+
+    def __init__(self):
+        self.error = ""
+
+    def log_error(self, user_key, message):
+        self.error = message
+
+    def get_error(self, user_key):
+        return self.error
+
+single_log = SingleLog()
+
+
 class BaseAlexaRequest(object):
+
+    BREAK = " <break /> "
 
     @property
     def intent(self):
@@ -15,8 +31,15 @@ class BaseAlexaRequest(object):
     def request_type(self):
         return self.event['request']['type']
 
-    def __init__(self, event):
+    @property
+    def user(self):
+        return self.event['session']['user']['userId']
+
+    def __init__(self, event, is_ssml=True, user_logger=single_log):
         self.event = event
+        self.is_ssml = is_ssml
+        self.user_logger = user_logger
+
         session = self.event.get("session", dict())
 
         self.session_attributes = session.get("attributes", dict())
@@ -49,12 +72,19 @@ class BaseAlexaRequest(object):
     def current_context(self):
         return self._context[0] if self._context else None
 
+    def format_output_speech(self, value):
+        output_speech = dict(type="SSML" if self.is_ssml else "PlainText")
+        if self.is_ssml:
+            wrap = "<speak>%s</speak>" % value
+            output_speech["ssml"] = wrap.replace(". ", self.BREAK).replace(", ", self.BREAK)
+        else:
+            output_speech["text"] = value
+
+        return output_speech
+
     def build_speechlet_response(self, title, response_text, reprompt_text=None):
         output = dict(
-            outputSpeech=dict(
-                type='PlainText',
-                text=response_text,
-            ),
+            outputSpeech=self.format_output_speech(response_text),
             card=dict(
                 type='Simple',
                 title=title,
@@ -73,16 +103,20 @@ class BaseAlexaRequest(object):
         return output
 
     def response(self):
-        if self.request_type == 'IntentRequest':
-            intent_name = self.intent_name.replace('.', '_')
-            try:
-                return getattr(self, intent_name)()
-            except Exception as e:
-                logging.info("Intent Not Implemented: %s" % intent_name)
-                raise e
-        elif self.request_type == "LaunchRequest":
-            return self.LaunchRequest()
-        return 'intentType: {s.request_type}'.format(s=self)
+        try:
+            if self.request_type == 'IntentRequest':
+                intent_name = self.intent_name.replace('.', '_')
+                try:
+                    return getattr(self, intent_name)()
+                except Exception as e:
+                    logging.warning(e)
+                    raise e
+            elif self.request_type == "LaunchRequest":
+                return self.LaunchRequest()
+            return 'intentType: {s.request_type}'.format(s=self)
+        except Exception as e:
+            self.user_logger.log_error(self.user, e.message)
+            return self.ErrorIntent()
 
     def LaunchRequest(self):
         return self.build_response(
@@ -96,7 +130,7 @@ class BaseAlexaRequest(object):
         self.clear_context()
         return self.build_response(
             speechletResponse=self.build_speechlet_response(
-                title='CancelIntent',
+                title='Cancel Intent',
                 response_text='goodbye',
             )
         )
@@ -116,5 +150,24 @@ class BaseAlexaRequest(object):
                 title='Help Intent',
                 response_text="Read the Manual.",
                 reprompt_text="R T F M"
+            )
+        )
+
+    def ErrorIntent(self, error):
+        return self.build_response(
+            speechletResponse=self.build_speechlet_response(
+                title='Error',
+                response_text="There was an error with the intent: %s." % self.intent_name,
+                reprompt_text="Say Diagnostic for details."
+            )
+        )
+
+    def DiagnosticIntent(self):
+        speech_output = self.user_logger.get_error(self.user) or "Nothing is wrong"
+        return self.build_response(
+            speechletResponse=self.build_speechlet_response(
+                title='Diagnostic',
+                response_text=speech_output,
+                reprompt_text="Does that help."
             )
         )
